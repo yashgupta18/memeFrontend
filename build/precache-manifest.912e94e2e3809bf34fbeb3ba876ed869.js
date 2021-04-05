8ba3d03ff3ab9abe3451f4500883eb4e52e200af
Yash Gupta <18guptayash@gmail.com>
2021-04-05 16:07:55 +0530

    client

---
+++ build/precache-manifest.912e94e2e3809bf34fbeb3ba876ed869.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9244b2ac16c348c762f34c76c316d01",
+    "url": "/yashgupta18/memeShare.git/index.html"
+  },
+  {
+    "revision": "11aa83e4cbc71ea318d5",
+    "url": "/yashgupta18/memeShare.git/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "14f3f19e6e5daf6010f6",
+    "url": "/yashgupta18/memeShare.git/static/js/2.1fe2160c.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/yashgupta18/memeShare.git/static/js/2.1fe2160c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11aa83e4cbc71ea318d5",
+    "url": "/yashgupta18/memeShare.git/static/js/main.bf617307.chunk.js"
+  },
+  {
+    "revision": "17e9a4a83b37d05301a1",
+    "url": "/yashgupta18/memeShare.git/static/js/runtime-main.2e85c3cc.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/yashgupta18/memeShare.git/static/media/memories.5c0c63fb.png"
+  }
+]);
