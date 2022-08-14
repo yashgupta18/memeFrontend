77eb9dcdec265ea385c067546551a31fadc760bc
yash <yash.gupta@hbkworld.com>
2022-08-14 14:10:24 +0530

    update proxy

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000" });
+const API = axios.create({ baseURL: "http://localhost:5001" });
 
 // happens on each req
 API.interceptors.request.use((req) => {
